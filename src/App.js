51b6d18662a8d2d0093d7a9660e5f6e35703e3c2
Mert Çelik <mertcelik29111@gmail.com>
2019-05-22 23:00:53 +0300

    first components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import User from "./components/User";
+import Navbar from "./components/Navbar";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h4 style = {{color : "red", fontSize:"30px"}}>App Component</h4>
+        <Navbar></Navbar>
+        <User></User>
+
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>İsim : Mert Çelik</li>
+                    <li>Departman : IT</li>
+                    <li>Maaş : 5000TL</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
