c742adb8c4089a214e237111dc6b2d8c7b6d2851
Mert Çelik <mertcelik29111@gmail.com>
2019-05-30 01:30:04 +0300

    some design items added.

--- src/components/User.js
+++ src/components/User.js
@@ -8,12 +8,18 @@ export default class User extends Component {
         //Destructing
         const {name, department, salary} = this.props;
         return (
-            <div>
-                <ul>
-                    <li>İsim : {name}</li>
-                    <li>Departman : {department}</li>
-                    <li>Maaş : {salary} </li>
-                </ul>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className = "card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className="fa fa-trash-alt" style = {{cursor : "pointer"}}></i>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text"> Maaş : {salary}</p>
+                        <p className="card-text"> Departman : {department}</p>
+                    </div>
+                </div>
+                
             </div>
         )
     }
