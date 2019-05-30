35b63fddd3de24b131596854bc8df02e7a565780
Mert Çelik <mertcelik29111@gmail.com>
2019-05-31 00:52:29 +0300

    state created

--- src/components/User.js
+++ src/components/User.js
@@ -3,21 +3,43 @@ import PropTypes from "prop-types";
 
 
 export default class User extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isVisible : false
+        }
+    }
+    /*
+    //this.onClickEvent.bind(this,34) şeklinde bind edip,aşağıdaki gibi değeri döndürebilirsin.
+    onClickEvent(number,event){
+        console.log(number);
+    }
+    */
+    onClickEvent(event){
+        console.log(this);
+    }
     render() {
 
         //Destructing
         const {name, department, salary} = this.props;
+        const {isVisible} = this.state;
         return (
             <div className="col-md-8 mb-4">
                 <div className="card">
                     <div className = "card-header d-flex justify-content-between">
-                        <h4 className="d-inline">{name}</h4>
+                        <h4 className="d-inline" onClick = {this.onClickEvent.bind(this)}>{name}</h4> 
                         <i className="fa fa-trash-alt" style = {{cursor : "pointer"}}></i>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text"> Maaş : {salary}</p>
-                        <p className="card-text"> Departman : {department}</p>
-                    </div>
+                    {
+                        isVisible ? 
+                        <div className="card-body">
+                            <p className="card-text"> Maaş : {salary}</p>
+                            <p className="card-text"> Departman : {department}</p>
+                        </div> : null
+                    }
+                    
                 </div>
                 
             </div>
